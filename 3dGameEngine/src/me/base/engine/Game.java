192f81be6df39c0c947c8973322a1acc7ef4b3c9
BuildTools <unconfigured@null.spigotmc.org>
2020-08-18 20:03:20 -0400

    Added mouse input

--- 3dGameEngine/src/me/base/engine/Game.java
+++ 3dGameEngine/src/me/base/engine/Game.java
@@ -14,6 +14,12 @@ public class Game {
 		
 		if(Input.getKeyUp(Keyboard.KEY_UP))
 			System.out.println("Released up");
+		
+		if(Input.getMouseDown(1))
+			System.out.println("Pressed mouse");
+		
+		if(Input.getMouseUp(1))
+			System.out.println("Released mouse");
 	}
 	
 	public void update() 
--- 3dGameEngine/src/me/base/engine/Input.java
+++ 3dGameEngine/src/me/base/engine/Input.java
@@ -3,17 +3,36 @@ package me.base.engine;
 import java.util.ArrayList;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 public class Input {
 	
 	public static final int NUM_KEYCODES = 256;
+	public static final int NUM_MOUSEBUTTONS = 5;
 	
 	private static ArrayList<Integer> currentKeys = new ArrayList<Integer>();
 	private static ArrayList<Integer> downKeys = new ArrayList<Integer>();
 	private static ArrayList<Integer> upKeys = new ArrayList<Integer>();
 	
+	private static ArrayList<Integer> currentMouse = new ArrayList<Integer>();
+	private static ArrayList<Integer> downMouse = new ArrayList<Integer>();
+	private static ArrayList<Integer> upMouse = new ArrayList<Integer>();
+	
 	public static void update() 
 	{
+		
+		upMouse.clear();
+		
+		for(int i = 0; i < NUM_MOUSEBUTTONS; i++)
+			if(!getMouse(i) && currentMouse.contains(i))
+				upMouse.add(i);
+		
+		downMouse.clear();
+		
+		for(int i = 0; i < NUM_MOUSEBUTTONS; i++)
+			if(getMouse(i) && !currentMouse.contains(i))
+				downMouse.add(i);
+		
 		upKeys.clear();
 		
 		for(int i = 0; i < NUM_KEYCODES; i++)
@@ -31,6 +50,12 @@ public class Input {
 		for(int i = 0; i < NUM_KEYCODES; i++)
 			if(getKey(i))
 				currentKeys.add(i);
+		
+		currentMouse.clear();
+		
+		for(int i =0; i < NUM_MOUSEBUTTONS; i++)
+			if(getMouse(i))
+				currentMouse.add(i);
 	}
 	
 	public static boolean getKey(int keyCode) 
@@ -40,20 +65,32 @@ public class Input {
 	
 	public static boolean getKeyDown(int keyCode)
 	{
-		if(downKeys.contains(keyCode))
-			return true;
-		
-		return false;
+		return downKeys.contains(keyCode);
 	}
 	
 	public static boolean getKeyUp(int keyCode)
 	{
-		if(upKeys.contains(keyCode))
-			return true;
-		
-		return false;
+		return upKeys.contains(keyCode);
+	}
+	
+	public static boolean getMouse(int mouseButton)
+	{
+		return Mouse.isButtonDown(mouseButton);
+	}
+	
+	public static boolean getMouseDown(int mouseButton)
+	{
+		return downMouse.contains(mouseButton);
+	}
+	
+	public static boolean getMouseUp(int mouseButton)
+	{
+		return upMouse.contains(mouseButton);
 	}
 }
+
+
+
 
 
 
